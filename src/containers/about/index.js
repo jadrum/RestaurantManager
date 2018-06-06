fb8b099104261657ac5b37f76f85b7dbdb38c0f3
Justin Drum <justindrum@justins-mbp.attlocal.net>
2018-06-06 17:23:10 -0400

    Changing styling and adding content to the unplanned webpages

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Button, Grid, Jumbotron, Row } from 'react-bootstrap';
 
 const About = () => (
   <div>
-    <h1>About Page</h1>
-    <p>Did you get here via Redux?</p>
+    <Grid>
+      <Row>
+        <Jumbotron>
+          <h1>JDB Burger Bar</h1>
+          <p>
+            This is a restaurant pos system app currently being developed by
+            Justin Drum, Dylan Drum, and Brandon Summers. Come take a look at
+            our work on
+            <Button bsStyle="link" bsSize="lg" onClick={this.openGitHub}>
+              GitHub
+            </Button>
+          </p>
+        </Jumbotron>
+      </Row>
+    </Grid>
   </div>
 );
 
